8bd909d62d9c9c445fcda31e53982287cce232b5
Karl Gavillot <ksgavillot@hotmail.com>
2022-03-15 10:50:13 +0100

    output file symptomes triés par ordre alphabétique et chaque symptome sur une ligne

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,46 +1,97 @@
 package com.hemebiotech.analytics;
 
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	/**
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		// la key représente un symptome et Integer représente l'occurence = compteur
+		HashMap<String, Integer> wordMap = new HashMap<String, Integer>();
+		// BufferedReader va aider à lire le fichier symptoms.txt,
+		// des exception peuvent survenir bonne pratique de l'entourer avec blocs try catch
+		// BufferReader a dans son constructeur la class FileReader
+		BufferedReader reader = null;
+		try{
+			// first get input
+			reader = new BufferedReader (new FileReader("symptoms.txt"));
 
-		//int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			//i++;	// increment i
-			//System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			// Maintenant le programme doit lire le fichier symptoms.txt ligne par ligne avec le type String
+			// Bonne partique rajouter un bloc catch si des exception surviennent avec la méthode readLine()
+			String line = reader.readLine();
+			// tant que on arrive pas sur une ligne null cad aucun mot sur la ligne
+			while(line != null) {
+				// si on avais une erreur sur le fichier symptoms.txt avec plusieurs symptomes sur la meme ligne avec des espace
+				// toLowerCase nous permet de mettre en minuscule tous les mots trouvé sur une ligne afin qu'un symptoms avec ou sans majuscule soit bien comptabilé
+				// la méthode split() permet d'enlever les espace pour ne garder que les mots
+				// que l'on stock dans une variable Array de type String
+				String words[] = line.toLowerCase().split(" ");
+
+				//Pour lire ces mots (symptomes) un par un dans le Array on utilise une boucle for
+				for(String word : words){
+					// si le mot (symptome) = key  est déja dans le HashMap
+					if(wordMap.containsKey(word)){
+						// get permet de retrouver ce mot (symptome key) dans le HashMap ensuite rajoute plus 1 au compteur (value)
+						wordMap.put(word, wordMap.get(word)+1);
+					}else{
+						// alors si le symptom trouvé n'est pas dans le HashMap, on rajoute le mot avec l'occurence 1
+						wordMap.put(word, 1);
+					}
+				}
+				// a ce stade nous n'avons lu que la premiére ligne de symptom.txt
+				//on doit déplacer le cursseur à la ligne suivante
+				line = reader.readLine(); // get another symptom
 			}
 
-			line = reader.readLine();	// get another symptom
-
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		} catch(IOException e){
+			e.printStackTrace();
 		}
-		System.out.println("number of headaches: " + headCount);
-		System.out.println("number of rush: " + rashCount);
-		System.out.println("number of pupils: " + pupilCount);
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		reader.close();
-		writer.close();
+
+
+		finally {
+			// Une foi la lecture du de symptoms.txt terminé on ferme le flux de lecture avec ce fichier
+			// et quand on le ferme reader des exception peuvent survenir, bonne pratique de l'entourer avec blocs try catch
+			try{
+				reader.close();
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		// affiche le décompte des symptomes dans la console
+		wordMap.forEach((k,v) -> System.out.println(k + " : " + v));
+
+		TreeMap<String, Integer> triMap = new TreeMap<>();
+		triMap.putAll(wordMap);
+
+		// affiche le décompte des symptomes par ordre alphabétique dans la console
+		System.out.println("_________________________________________________________ Sorted List \n");
+		triMap.forEach((k,v) -> System.out.println(k + " : " + v));
+
+		int SymptomsTotal = 0;
+		try{
+			// next generate output
+			FileWriter writer = new FileWriter ("result.out");
+			//writer.write(String.valueOf(wordMap));
+			for ( Map.Entry<String, Integer> entry  : triMap.entrySet()) {
+				writer.write(entry.getKey() + " = " + entry.getValue() + "\n");
+				SymptomsTotal += entry.getValue();
+			}
+			writer.write("Symptoms variations= " + triMap.size() + " Total of symptoms = " + SymptomsTotal);
+			writer.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
